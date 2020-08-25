c0cb4180feb3e57fdc296faddaabcb16ec295a64
Christine Ross <christinejoyross@gmail.com>
2020-08-25 13:04:59 -0700

    display single question

--- script.js
+++ script.js
@@ -1,97 +1,127 @@
-function buildQuiz() {
+(function () {
+    function buildQuiz() {
 
-    const output = [];
+        const output = [];
 
-    myQuestions.forEach((currentQuestion, questionNumber) => {
+        myQuestions.forEach((currentQuestion, questionNumber) => {
 
-        //create variable to store the list of possile answers
-        const answers = [];
+            //create variable to store the list of possile answers
+            const answers = [];
 
-        //for each of the answer choicess
-        for (letter in currentQuestion.answers) {
+            //for each of the answer choicess
+            for (letter in currentQuestion.answers) {
 
-            //create an HTML radio button
-            answers.push(
-                `<label> <input type= "radio" name= "question${questionNumber}" value= "${letter}"> ${letter} : ${currentQuestion.answers[letter]} </label>`
-            );
-        }
+                //create an HTML radio button
+                answers.push(
+                    `<label> <input type= "radio" name= "question${questionNumber}" value= "${letter}"> ${letter} : ${currentQuestion.answers[letter]} </label>`
+                );
+            }
 
-        console.log(answers);
+            
 
-        output.push(
-            `<div class="question"> ${currentQuestion.question} </div>
-            <div class = "answers"> ${answers.join('')} </div>`
+            // console.log(output);
 
-        );
+            output.push(
+                `<div class="slide">
+                <div class="question"> ${currentQuestion.question}</div>
+                <div class="answers"> ${answers.join("")}</div>
+            </div>`
+            )
 
-        console.log(output);
+        })
 
-    })
-
-    quizContainer.innerHTML = output.join('');
-
-};
-
-function showResults() {
-
-    const answerContainers = quizContainer.querySelectorAll('.answers');
-
-    let numCorrect = 0;
-
-    myQuestions.forEach((currentQuestion, questionNumber) => {
-        const answerContainer = answerContainers[questionNumber];
-        const selector = `input[name=question${questionNumber}]:checked`;
-        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
-        if (userAnswer === currentQuestion.correctAnswer) {
-            numCorrect++
-
-            answerContainers[questionNumber].getElementsByClassName.color = 'lightgreen';
-        } else {
-            answerContainers[questionNumber].getElementsByClassName.color = 'red';
-        }
-
-    })
-
-    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+        quizContainer.innerHTML = output.join('');
 
 
-};
+
+    };
+
+    function showResults() {
+
+        const answerContainers = quizContainer.querySelectorAll('.answers');
+
+        let numCorrect = 0;
+
+        myQuestions.forEach( (currentQuestion, questionNumber) => {
+            const answerContainer = answerContainers[questionNumber];
+            const selector = `input[name=question${questionNumber}]:checked`;
+            const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+            if (userAnswer === currentQuestion.correctAnswer) {
+                numCorrect++;
+
+                answerContainers[questionNumber].style.color = 'lightgreen';
+            } else {
+                answerContainers[questionNumber].style.color = 'red';
+            }
+        });
+        resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
 
 
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
-
-const myQuestions = [
-    {
-        question: "What is the best dog breed?",
-        answers: {
-            a: "Poodle",
-            b: "Beagle",
-            c: "Pitbull"
-        },
-        correctAnswer: "a"
-    },
-    {
-        question: "What is the water brand",
-        answers: {
-            a: "Dasani",
-            b: "Aquafina",
-            c: "Kroger"
-        },
-        correctAnswer: "b"
-    },
-    {
-        question: "What is the best phone brand",
-        answers: {
-            a: "Windows",
-            b: "Samsung",
-            c: "Apple"
-        },
-        correctAnswer: "c"
     }
-];
+
+    function showSlide(n) {
+        slides[currentSlide].classList.remove('active-slide');
+        slides[n].classList.add('active-slide');
+
+        currentSlide = n;
+        if (currentSlide === 0) {
+            previousButton.style.display = 'none';
+        } else {
+            previousButton.style.display = 'inline-block';
+        }
+
+        if (currentSlide === slides.length - 1) {
+            nextButton.style.display = 'none';
+            submitButton.style.display = 'inline-block'
+        } else {
+            nextButton.style.display = 'inline-block';
+            submitButton.style.display = 'none';
+        }
+    }
+
+    function showNextSlide() {
+        showSlide(currentSlide + 1);
+    }
+
+    function showPreviousSlide() {
+        showSlide(currentSlide - 1);
+    };
+
+
+    const quizContainer = document.getElementById('quiz');
+    const resultsContainer = document.getElementById('results');
+    const submitButton = document.getElementById('submit');
+
+    const myQuestions = [
+        {
+            question: "What is the best dog breed?",
+            answers: {
+                a: "Poodle",
+                b: "Beagle",
+                c: "Pitbull"
+            },
+            correctAnswer: "a"
+        },
+        {
+            question: "What is the water brand",
+            answers: {
+                a: "Dasani",
+                b: "Aquafina",
+                c: "Kroger"
+            },
+            correctAnswer: "b"
+        },
+        {
+            question: "What is the best phone brand",
+            answers: {
+                a: "Windows",
+                b: "Samsung",
+                c: "Apple"
+            },
+            correctAnswer: "c"
+        }
+    ];
 
 
 
@@ -99,10 +129,26 @@ const myQuestions = [
 
 
 
-//call on buildquiz function to display quiz right away
+    //call on buildquiz function to display quiz right away
 
-buildQuiz();
+    buildQuiz();
 
-//show results when submit button is clicked
-submitButton.addEventListener('click', showResults);
+    //pagination code 
+    const previousButton = document.getElementById("previous");
+    const nextButton = document.getElementById("next");
+    const slides = document.querySelectorAll(".slide");
+    let currentSlide = 0;
 
+
+
+    //show first slide
+    showSlide(currentSlide)
+
+    // showSlide(0)
+
+
+    submitButton.addEventListener('click', showResults);
+    previousButton.addEventListener("click", showPreviousSlide);
+    nextButton.addEventListener("click", showNextSlide);
+
+})();
