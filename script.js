fe8a59ea969050ad026db169cb44b01b7e611f79
Christine Ross <christinejoyross@gmail.com>
2020-08-25 11:41:12 -0700

    modified entire quiz

--- script.js
+++ script.js
@@ -1,43 +1,94 @@
-var timer = 75;
-var timerEl = document.getElementById('timer');
-const startButton = document.getElementById('start-btn');
-const welcomeEl = document.getElementById('welcome');
-const quizEl = document.getElementById('the-questions');
-const questionsEl = document.getElementById('question');
-const answerButtonEl = document.getElementById('answer-buttons');
+function buildQuiz() {
 
+    const output= [];
 
-//functions
+    myQuestions.forEach((currentQuestion, questionNumber) => {
 
-function startQuiz() {
-    console.log('started');
-    welcomeEl.classList.add('hide');
-    startButton.classList.add('hide');
-    quizEl.classList.remove('hide');
-    showQuestion();
+        //create variable to store the list of possile answers
+        const answers = [];
 
-};
+        //for each of the answer choicess
+        for (letter in currentQuestion.answers) {
 
-function showQuestion(){
-    const content=
-        {
-            question:'What is this?',
-            answers: [
-                {text:'option1', correct: true},
-                {text:'option2', correct: false}
-            ]
-        };    
-    
-    console.log(content.question);
+            //create an HTML radio button
+            answers.push(
+                `<label> <input type= "radio" name= "question${questionNumber}" value= "${letter}"> ${letter} : ${currentQuestion.answers[letter]} </label>`
+            );
+        }
 
-    questionsEl.textContent = content.question;
+        console.log(answers);
+
+        output.push(
+            `<div class="question"> ${currentQuestion.question} </div>
+            <div class = "answers"> ${answers.join('')} </div>`
+
+        );
+
+        console.log(output);
+
+    })
+
+    quizContainer.innerHTML = output.join('');
 
 };
 
+function showResults(){
+
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+
+    let numCorrect = 0;
+
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+        const answerConstainer = answerContainers
+    })
+
+};
+
+
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+
+const myQuestions = [
+    {
+        question: "What is the best dog breed?",
+        answers: {
+            a: "Poodle",
+            b: "Beagle",
+            c: "Pitbull"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "What is the water brand",
+        answers: {
+            a: "Dasani",
+            b: "Aquafina",
+            c: "Kroger"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "What is the best phone brand",
+        answers: {
+            a: "Windows",
+            b: "Samsung",
+            c: "Apple"
+        },
+        correctAnswer: "c"
+    }
+];
 
 
 
 
-//event listeners 
 
-startButton.addEventListener("click", startQuiz);
+
+
+//call on buildquiz function to display quiz right away
+
+buildQuiz();
+
+//show results when submit button is clicked
+submitButton.addEventListener('click', showResults);
+
